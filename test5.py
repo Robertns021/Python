f483e51bc320b7898545b0a4c2403e2d6cc493bc
Robert <molnarrobert99@yahoo.com>
2024-11-09 12:48:11 +0100

    Simple for loop

---
+++ test5.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+sum = 0
+
+for num in numbers:
+    sum += num
+
+print(f"The sum of all the numbers is: {sum}")
+
+print("The first 5 numbers in list: ")
+for i in range(0, 5):
+    print(numbers[i])
+    
