97d9ba7322b7115a66a6a8e3b3c7b635180f8140
Robert <molnarrobert99@yahoo.com>
2024-11-26 18:16:39 +0100

    List methods

---
+++ test7.py
@@ -0,0 +1,12 @@
+num = [1, 4, 6, 2, 3]
+num.insert(0, 10)   #add 10 to index 0
+num.remove(4)       #remove 4 from list
+num.append(11)      #add 11 to end 
+#num.index(6)       #gives index of first 6 in list
+#num.count()        #count of list
+num.sort()          #sort list
+num.reverse()       #reverses list
+
+num_copy = num.copy()   #makes a copy of the list
+
+print(num)
